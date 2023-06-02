4a99bdde81a857e2dddc0f7275bf14a64aa4abf3
dimacv <47975357+dimacv@users.noreply.github.com>
2023-06-02 15:09:29 +0300

    Update monitoring_with_sound.cmd

--- monitoring_with_sound.cmd
+++ monitoring_with_sound.cmd
@@ -1,33 +1,24 @@
-`python
-import subprocess
-import time
-import winsound
+@echo off
+set hostname=10.45.1.20
+set DOWN_CNT=5
+set /a cnt1=0
+set /a cnt2=0
 
-hostname = "10.45.1.20" # замените на ваш адрес сервера
-
-DOWN_CNT = 5
-cnt1 = cnt2 = 0
-
-while True:
-    response = subprocess.Popen(['ping', '-n', '1', hostname], stdout=subprocess.PIPE).stdout.read()
-    response_str = response.decode('cp866') # декодируем байтовую строку в строковую
-    #print(response)
-    if "TTL=" not in response_str: # если ответ содержит такую строку, значит пинг выполнен успешно
-        print(hostname + " -> Server is down!!!!!!!!!!! " + str(cnt1))
-        cnt1 += 1
-        if cnt1 >= DOWN_CNT:
-            # заменить на нужный звук
-            winsound.PlaySound("C:/Windows/Media/Alarm10.wav", winsound.SND_ASYNC)
-            #time.sleep(1)
-
-
-    else:
-        print(hostname + " -> Server is up! " + str(cnt2))
-        if cnt2 >= DOWN_CNT:
-            cnt2 = 0
-        else:
-            cnt2 += 1
-        cnt1 = 0
-`
-переделай этот код на такой же код пакетного cmd-файла для windows.
-подробно прокомментируй его построчно на русском.
+:loop
+ping -n 1 %hostname% | find "TTL=" >nul
+if errorlevel 1 (
+    echo %hostname% -^> Server is down!!!!!!!!!!! %cnt1%
+    set /a cnt1+=1
+    if %cnt1% geq %DOWN_CNT% (
+        rem Replace with desired sound file
+        start "" "C:\Windows\Media\Alarm10.wav"
+    )
+) else (
+    echo %hostname% -^> Server is up! %cnt2%
+    set /a cnt2+=1
+    if %cnt2% geq %DOWN_CNT% (
+        set /a cnt2=0
+    )
+    set /a cnt1=0
+)
+goto loop

003be4e2502da25a1e25595b4f8f0d002f562fae
dimacv <47975357+dimacv@users.noreply.github.com>
2023-06-02 15:08:38 +0300

    Update monitoring_with_sound.cmd

--- monitoring_with_sound.cmd
+++ monitoring_with_sound.cmd
@@ -1,39 +1,33 @@
-@echo off
-setlocal enabledelayedexpansion
+`python
+import subprocess
+import time
+import winsound
 
-set "hostname=10.45.1.20"  REM Замените на свой адрес сервера
+hostname = "10.45.1.20" # замените на ваш адрес сервера
 
-set "DOWN_CNT=5"
-set "cnt1=0"
-set "cnt2=0"
+DOWN_CNT = 5
+cnt1 = cnt2 = 0
 
-:loop
-REM Выполнение команды ping с одной попыткой и проверкой вывода
-for /f "tokens=*" %%A in ('ping -n 1 %hostname%') do (
-    REM Проверка наличия строки "TTL=" в выводе команды
-    echo %%A | find "TTL=" >nul
-    if errorlevel 1 (
-        REM Если ответ не содержит строку "TTL=", значит сервер недоступен
-        echo %hostname% -> Сервер недоступен! ! ! ! ! ! ! %cnt1%
-        set /a cnt1+=1
-        if %cnt1% gtr %DOWN_CNT% (
-            REM Заменить на нужный звук
-            start "" "C:\Windows\Media\Alarm10.wav"
-            REM Добавьте задержку времени, если требуется
-            REM timeout /t 1 /nobreak >nul
-        )
-    ) else (
-        REM Если ответ содержит строку "TTL=", значит сервер доступен
-        echo %hostname% -> Сервер доступен! %cnt2%
-        if %cnt2% gtr %DOWN_CNT% (
-            set "cnt2=0"
-        ) else (
-            set /a cnt2+=1
-        )
-        set "cnt1=0"
-    )
-)
+while True:
+    response = subprocess.Popen(['ping', '-n', '1', hostname], stdout=subprocess.PIPE).stdout.read()
+    response_str = response.decode('cp866') # декодируем байтовую строку в строковую
+    #print(response)
+    if "TTL=" not in response_str: # если ответ содержит такую строку, значит пинг выполнен успешно
+        print(hostname + " -> Server is down!!!!!!!!!!! " + str(cnt1))
+        cnt1 += 1
+        if cnt1 >= DOWN_CNT:
+            # заменить на нужный звук
+            winsound.PlaySound("C:/Windows/Media/Alarm10.wav", winsound.SND_ASYNC)
+            #time.sleep(1)
 
-REM Задержка времени, прежде чем повторить цикл
-timeout /t 1 /nobreak >nul
-goto loop
+
+    else:
+        print(hostname + " -> Server is up! " + str(cnt2))
+        if cnt2 >= DOWN_CNT:
+            cnt2 = 0
+        else:
+            cnt2 += 1
+        cnt1 = 0
+`
+переделай этот код на такой же код пакетного cmd-файла для windows.
+подробно прокомментируй его построчно на русском.
